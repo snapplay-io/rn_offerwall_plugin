dfd9ac3406df729fa8c7a779ffcf3c88d7f5e3e1
dio71 <dio71.kim@gmail.com>
2025-11-14 09:08:39 +0900

    chore: initial commit

---
+++ ios/S2offerwall.h
@@ -0,0 +1,5 @@
+#import <S2offerwallSpec/S2offerwallSpec.h>
+
+@interface S2offerwall : NSObject <NativeS2offerwallSpec>
+
+@end
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import S2offerwall from './NativeS2offerwall';
+
+export function multiply(a: number, b: number): number {
+  return S2offerwall.multiply(a, b);
+}
